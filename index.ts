e8fe4bc70a446ae2f130409c4e1ea5c5c019424e
a bad dev <dylan@tritan.dev>
2022-10-28 07:32:35 +0100

    uwu

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import config from "./config";
 
 export default async function init() {
   console.log("Launching browser");
-  debugger;
+
   let browser = await puppeteer.launch({
     headless: true,
     executablePath: "/usr/bin/chromium-browser",
@@ -14,17 +14,12 @@ export default async function init() {
   new Promise((r) => setTimeout(r, 1000));
 
   console.log("Visiting website");
-  debugger;
   await page.goto("https://www.omegle.com");
-
   await page.focus(".newtopicinput");
+
   await page.keyboard.type(`${config.keyword}`);
   await page.keyboard.press("Enter");
 
-  // await page.$eval(
-  //   ".newtopicinput",
-  //   (el: any) => (el.value = config.keyword as string)
-  // );
   await page.click("#textbtn");
 
   const elHandleArray = await page.$$("div div p label");
@@ -36,7 +31,6 @@ export default async function init() {
   const confirm = await page.$$("div div p input");
   var d = 0;
 
-  debugger;
   console.log("Confirming chat request");
   for (const el of confirm) {
     d++;
@@ -46,20 +40,17 @@ export default async function init() {
   await page.waitForTimeout(2000);
 
   do {
-    debugger;
     let randomMessage = config.messages[
       Math.floor(Math.random() * config.messages.length)
     ] as string;
 
     console.log("Sending message");
-    // await page.$eval(".chatmsg ", (el: any) => (el.value = randomMessage));
     await page.focus(".chatmsg");
     await page.keyboard.type(`${randomMessage}`);
     await page.keyboard.press("Enter");
 
     new Promise((r) => setTimeout(r, 2000));
 
-    debugger;
     console.log("Disconnecting");
     await page.click(".disconnectbtn");
     await page.click(".disconnectbtn");
@@ -67,12 +58,11 @@ export default async function init() {
 
     while ((await page.$(".sendbtn[disabled]")) !== null) {
       new Promise((r) => setTimeout(r, 3000));
-      debugger;
       await page.click(".disconnectbtn");
+      break;
     }
-
-    init();
   } while (true);
+  init();
 }
 
 init();
