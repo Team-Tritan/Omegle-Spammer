5483665509d9ef58efde770ce05ea5d715a061e5
a bad dev <dylan@tritan.dev>
2022-10-28 07:24:07 +0100

    uwu

---
+++ dist/config.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const config = {
+    keyword: "sqas98fas908fashfai9u8fhaiuhf",
+    messages: [
+        "free esex . gg /s5mBfRqyZs",
+        "big tiddy whores . gg/s5mBfRqyZs",
+        "sugar mommys! .gg/ s5mBfRqyZs",
+        "get whored .gg/s5mBf RqyZs",
+        "free anal vids s5mBfRqyZs",
+        "cumzone s5mBfRqyZs",
+    ],
+};
+exports.default = config;
