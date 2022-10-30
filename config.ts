8e9431fb318a9c074cd3e4e436857ef24c90f501
a bad dev <dylan@tritan.dev>
2022-10-30 09:29:19 -0400

    AAAAAAAAAA IT WORKS AND PROXIES WITH CAPTCHAS

--- config.ts
+++ config.ts
@@ -1,20 +1,30 @@
 const config = {
   chrome_exe_path:
     "C:\\Users\\Administrator.SADMIN\\Desktop\\Chrome\\chrome-win\\chrome.exe",
-  user_agent: ["Chrome"],
+  user_agent: ["Cumzone fuck"],
+
   _2captchaKey: "0b1e4659fbe0e6a639f7616b86d9c28a",
 
-  keyword: ["discord"],
+  keyword: ["discord"], // only do one or u can figure out how to fix text input for array
   messages: [
     "free esex . gg /s5mBfRqyZs",
     "big tiddy whores . gg/s5mBfRqyZs",
     "sugar mommys! .gg/ s5mBfRqyZs",
     "get whored .gg/s5mBf RqyZs",
-    "free anal vids s5mBfRqyZs",
     "cumzone s5mBfRqyZs",
+    "get the source of this bot -> s5mBfRqyZs",
+    "we will fuck you s5mBfRqyZs",
+    "cute men s5mBfRqyZs",
+    "cute trans girls s5mBfRqyZs",
+    "cute femboys discord gg / s5mBfRqyZs",
+    "hot devs .gg/s5mBfRqyZs",
+    "we're not fucking assholes .gg/s5mBfRqyZs",
+    "ur mom, .gg/s5mBfRqyZs",
   ],
 
   headless: false,
+  proxy_url:
+    "http://pro120rbrcwt223127:jOrY08qmpTccEOCJ@rip.proxies.yachts:9988",
 };
 
 export default config;
