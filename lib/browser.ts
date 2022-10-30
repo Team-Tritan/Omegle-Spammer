42d3d784e250837b5d4784ce24b4a9793c533391
a bad dev <dylan@tritan.dev>
2022-10-30 09:28:22 -0400

    converted to classes

---
+++ lib/browser.ts
@@ -0,0 +1,45 @@
+import config from "../config";
+import puppeteer from "puppeteer-extra";
+import RecaptchaPlugin from "puppeteer-extra-plugin-recaptcha";
+import constructChat from "./tags";
+import handleChat from "./chat";
+
+export default class OmegleFuery {
+  page: any;
+  browser: any;
+  data!: {
+    ips: [];
+  };
+
+  constructor() {
+    this.initPlugins();
+    this.buildBrowser();
+  }
+
+  async initPlugins() {
+    puppeteer.use(
+      RecaptchaPlugin({
+        provider: {
+          id: "2captcha",
+          token: config._2captchaKey,
+        },
+        visualFeedback: true,
+      })
+    );
+  }
+
+  async buildBrowser() {
+    console.log("Launching browser");
+
+    this.browser = await puppeteer.launch({
+      headless: config.headless,
+      executablePath: config.chrome_exe_path,
+      args: ["--disable-web-security"],
+    });
+
+    this.page = await this.browser.newPage();
+
+    await constructChat(this.page);
+    await handleChat(this.page);
+  }
+}
