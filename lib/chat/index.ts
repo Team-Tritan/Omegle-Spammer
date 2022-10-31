c8ce4a066ba9238ece4a4aaf6e45682b643c161e
a bad dev <dylan@tritan.dev>
2022-10-31 07:24:47 -0400

    oops

--- lib/chat/index.ts
+++ lib/chat/index.ts
@@ -2,15 +2,16 @@ import config from "../../config";
 
 export default async function handleChat(page: any) {
   console.log("Waiting for DOM load");
-  await new Promise((r) => setTimeout(r, 3000));
+  await new Promise((r) => setTimeout(r, 2000));
 
   console.log("Connecting to new stranger");
   let bench = Date.now();
 
   console.log("Checking/solving captchas");
   await page.solveRecaptchas();
-  await new Promise((r) => setTimeout(r, 1000));
   console.log("Connected after", Date.now() - bench + " ms");
+
+  await new Promise((r) => setTimeout(r, 2000));
 
   let randomMessage = config.messages[
     Math.floor(Math.random() * config.messages.length)
@@ -21,12 +22,8 @@ export default async function handleChat(page: any) {
   await page.keyboard.type(`${randomMessage}`);
   await page.keyboard.press("Enter");
 
-  new Promise((r) => setTimeout(r, 2000));
-
   console.log("Screenshotting");
   await page.screenshot({ path: `./tmp/${Date.now()}.png` });
-
-  await new Promise((r) => setTimeout(r, 2000));
 
   console.log("Disconnecting");
   await page.click(".disconnectbtn");
